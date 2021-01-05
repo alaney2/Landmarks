9d2874c4dac05f37bc562f1ee69737e92102589a
Alan Yao <alanyao.training@gmail.com>
2021-01-05 14:22:23 -0800

    starting categories

--- Landmarks/Views/Badges/Badge.swift
+++ Landmarks/Views/Badges/Badge.swift
@@ -8,9 +8,16 @@
 import SwiftUI
 
 struct Badge: View {
+    static let rotationCount = 8
+    
     var badgeSymbols: some View {
-        RotatedBadgeSymbol(angle: Angle(degrees: 0))
-            .opacity(0.5)
+        ForEach(0..<Badge.rotationCount) { i in
+            RotatedBadgeSymbol(
+                angle: .degrees(Double(i) / Double(Badge.rotationCount)) * 360.0
+            )
+                
+        }
+        .opacity(0.5)
     }
     
     var body: some View {
@@ -23,6 +30,7 @@ struct Badge: View {
                     .position(x: geometry.size.width / 2.0, y: (3.0 / 4.0) * geometry.size.height)
             }
         }
+        .scaledToFit()
     }
 }
 
