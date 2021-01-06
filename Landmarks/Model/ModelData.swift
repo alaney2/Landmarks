150ad68d873151fe4b3f1b740c94d4146b0d6408
Alan Yao <alanyao.training@gmail.com>
2021-01-05 23:50:11 -0800

    finished categories

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -10,10 +10,15 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    
     var categories: [String: [Landmark]] {
         Dictionary(
             grouping: landmarks, by: { $0.category.rawValue }
         )
+    }
+    
+    var features: [Landmark] {
+        landmarks.filter { $0.isFeatured }
     }
     
     var hikes: [Hike] = load("hikeData.json")
